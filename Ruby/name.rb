81c23c2203712f9430f5c72d46939eae98a34e5a
DashaRu <daluca@mail.ru>
2014-09-13 22:30:37 +0400

    names

---
+++ Ruby/name.rb
@@ -0,0 +1,34 @@
+puts '...ты можешь сказать это снова...'
+puts '...ты можешь сказать это снова...'
+
+myString = '...ты можешь сказать это снова...'
+puts myString
+puts myString
+
+name = 'Патриция Розанна Джессика Милдред Оппенгеймер'
+puts 'Меня зовут ' + name + '.'
+puts 'Ого! ' + name + ' - это правда длинное имя!'
+
+composer = 'Моцарт'
+puts composer + ' был "сенсацией" в своё время.'
+
+composer = 'Бетховен'
+puts 'Но мне лично больше нравится ' + composer + '.'
+
+var = 'ещё одна ' + 'строка'
+
+puts var
+
+var = 5 * (1+2)
+puts var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'восемь'
+puts var1
+puts var2
