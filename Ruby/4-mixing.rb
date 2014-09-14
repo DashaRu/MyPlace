fe7119aa777a4dcedc67c5fdf70ca4b1ef834e29
DashaRu <daluca@mail.ru>
2014-09-14 14:12:48 +0400

    error self

--- Ruby/4-mixing.rb
+++ Ruby/4-mixing.rb
@@ -22,3 +22,27 @@ puts ''
 puts 20
 puts 20.to_s
 puts '20'
+
+puts ''
+
+puts 'Приветик, ну и как Вас зовут?'
+name = gets.chomp
+puts puts 'Вас зовут ' + name + '? Какое прекрасное имя!'
+puts 'Рад познакомиться с Вами, ' + name + '. :)'
+
+puts ''
+
+puts 'Здравствуйте. Назовите Ваше имя.'
+name = gets.chomp
+puts 'Теперь отчество...'
+name1 = gets.chomp
+puts '... и фамилию.'
+name2 = gets.chomp
+puts 'Добро пожаловать, ' + name + ' ' + name1 + ' ' + name2 + '!'
+
+puts ''
+
+puts 'Добрый день! Назовите Ваше любимое число.'
+number = gets.chomp
+better = number.to_i + 1 
+puts better.to_s + ' - это большее и лучшее число.'
---
+++ Ruby/5-methods.rb
@@ -0,0 +1,5 @@
+puts 'привет '.+ 'мир'
+puts (10.*9).+ 9
+iCantBelieveIMadeAVariableNameThisLongJustToPointToA3 = 3
+puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
+self.puts iCantBelieveIMadeAVariableNameThisLongJustToPointToA3
