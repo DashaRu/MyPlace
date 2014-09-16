b154574e95396c7aa6dcb41b732215fe558a05d8
DashaRu <daluca@mail.ru>
2014-09-17 00:04:40 +0400

    add Leap Years

--- Ruby/6-flow.rb
+++ Ruby/6-flow.rb
@@ -112,3 +112,18 @@ puts 'Bye'
 
 puts ''
 
+puts 'Напишите начальный год'
+fir = gets.chomp.to_i
+puts 'Напишите конечный год'
+sec = gets.chomp.to_i
+puts ''
+year = fir
+while year != sec
+	if year % 4 == 0
+	puts year
+	end
+	year = year + 1
+end
+if sec % 4 == 0
+puts sec
+end
